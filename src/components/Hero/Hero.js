ecebd58a91713649f99787d7b3fbac90ebc67042
Jonathan P Rosso <jonathan.p.rosso@gmail.com>
2020-08-04 20:45:32 -0300

    Alteração h1 retornando - Aspas simples

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Hero = ({ title, imageSrc }) => {
   return (
@@ -6,24 +6,24 @@ const Hero = ({ title, imageSrc }) => {
       className="relative w-full overflow-hidden"
       style={{
         backgroundImage: `url(${imageSrc})`,
-        height: "350px",
-        backgroundAttachment: "fixed",
-        backgroundPosition: "botton center",
-        backgroundPositionX: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
+        height: '350px',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'botton center',
+        backgroundPositionX: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
       }}
     >
       <div className="text-center flex flex-row justify-center">
         <h1
-          className="mt-40 text-red-900 text-4xl px-10 font-bold"
-          style={{ textShadow: "1px 1px 4px rgba(34, 34, 34, 0.6)" }}
+          className="mt-40 text-white text-4xl px-10 font-bold"
+          style={{ textShadow: '1px 1px 4px rgba(34, 34, 34, 0.6)' }}
         >
           {title}
         </h1>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
